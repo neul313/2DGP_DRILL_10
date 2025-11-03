861ff84d035fb934f043535708476923972cbd79
Jo shinBe <tls3123@tukorea.ac.kr>
2025-11-03 21:22:00 +0900

    새 애니메이션 관련 변수 추가

--- bird.py
+++ bird.py
@@ -2,6 +2,15 @@ from pico2d import *
 import game_world
 import game_framework
 
+PIXEL_PER_METER = (1.0 / 0.03)
+RUN_SPEED_KMPH = 45.0 #참새의 평균 속도
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 3600.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 5
 
 class Bird:
     image = None
@@ -13,6 +22,7 @@ class Bird:
 
     def draw(self):
         self.image.clip_draw(0, 0, 150, 150, self.x, self.y,80,80)
+
 
     def update(self):
         if self.x > 1600:
